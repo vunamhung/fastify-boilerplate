612f08dcb42bb144d4ba361ba8475c58a34baf99
Hung Vu <vunamhung@gmail.com>
2020-09-23 05:15:55 +0700

    refactor code

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,12 @@ import * as fastify from "fastify";
 import * as fastifyBlipp from "fastify-blipp";
 import { Server, IncomingMessage, ServerResponse } from "http";
 import * as config from "config";
-import statusRoutes from "routes/status";
-import vehiclesRoutes from "routes/vehicles";
-import errorThrowerRoutes from "routes/error-thrower";
-import db from "db";
+import statusRoutes from "./routes/status";
+import vehiclesRoutes from "./routes/vehicles";
+import errorThrowerRoutes from "./routes/error-thrower";
+import db from "./db";
 
-const server: fastify.FastifyInstance<
-  Server,
-  IncomingMessage,
-  ServerResponse
-> = fastify({ logger: true });
+const server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({ logger: true });
 
 server.register(fastifyBlipp);
 server.register(db, config.get("db"));
@@ -24,7 +20,7 @@ server.register(errorThrowerRoutes);
 
 const start = async () => {
   try {
-    await server.listen(3000, "0.0.0.0");
+    await server.listen(3000, "localhost");
     server.blipp();
   } catch (err) {
     console.log(err);

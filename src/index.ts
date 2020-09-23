76109c7390b801acfc40427523a8400d24b8ee48
Hung Vu <vunamhung@gmail.com>
2020-09-23 11:31:56 +0700

    use dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as sourceMapSupport from "source-map-support";
 sourceMapSupport.install();
 
+require("dotenv").config();
+
 import * as fastify from "fastify";
 import * as fastifyBlipp from "fastify-blipp";
 import { Server, IncomingMessage, ServerResponse } from "http";
-import * as config from "config";
 import statusRoutes from "./routes/status";
 import vehiclesRoutes from "./routes/vehicles";
 import errorThrowerRoutes from "./routes/error-thrower";
@@ -13,7 +14,7 @@ import db from "./db";
 const server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({ logger: true });
 
 server.register(fastifyBlipp);
-server.register(db, config.get("db"));
+server.register(db, { uri: process.env.DB_URI });
 server.register(vehiclesRoutes);
 server.register(statusRoutes);
 server.register(errorThrowerRoutes);

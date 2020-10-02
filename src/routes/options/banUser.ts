7ca141132560a1dbeacc1810fbddbbff1431ffa4
Hung Vu <vunamhung@gmail.com>
2020-10-02 16:02:54 +0700

    banUser.ts

---
+++ src/routes/options/banUser.ts
@@ -0,0 +1,37 @@
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import { uniq } from 'ramda';
+import Option from '../../models/Option';
+
+export default function (server: FastifyInstance, options, done) {
+  server.put(
+    '/ban/:email',
+    {
+      preValidation: [server.authenticate, server.isAdmin],
+      schema: {
+        tags: ['options'],
+        security: [{ apiKey: [] }],
+        summary: 'Ban user by email.',
+        params: {
+          type: 'object',
+          properties: {
+            email: { type: 'string', format: 'email' },
+          },
+        },
+      },
+    },
+    async (request: FastifyRequest, reply) => {
+      // @ts-ignore
+      const { email } = request.params;
+
+      const banUsers = await Option.findOne({ name: 'ban_users' });
+
+      banUsers.data = [...banUsers.data, email];
+
+      await banUsers.save();
+
+      reply.send({ success: true, message: `User ${email} is banned.` });
+    },
+  );
+
+  done();
+}

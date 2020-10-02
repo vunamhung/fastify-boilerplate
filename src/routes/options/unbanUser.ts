3c1eee5e7acfeadf3fdf61ce36f537a6c54a2261
Hung Vu <vunamhung@gmail.com>
2020-10-02 15:59:01 +0700

    unbanUser.ts

---
+++ src/routes/options/unbanUser.ts
@@ -0,0 +1,37 @@
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import { uniq } from 'ramda';
+import Option from '../../models/Option';
+
+export default function (server: FastifyInstance, options, done) {
+  server.put(
+    '/unban/:email',
+    {
+      preValidation: [server.authenticate, server.isAdmin],
+      schema: {
+        tags: ['options'],
+        security: [{ apiKey: [] }],
+        summary: 'Unban user by email.',
+        params: {
+          type: 'object',
+          properties: {
+            email: { type: 'string', format: 'email' },
+          },
+        },
+      },
+    },
+    async (request: FastifyRequest, reply) => {
+      // @ts-ignore
+      const { email } = request.params;
+
+      const banUsers = await Option.findOne({ name: 'ban_users' });
+
+      banUsers.data = banUsers.data.filter((banEmail) => banEmail !== email);
+
+      await banUsers.save();
+
+      reply.send({ success: true, message: `User ${email} is unban.` });
+    },
+  );
+
+  done();
+}

08bc5e259a6475a3417aefc4f60a91b2078fe385
Hung Vu <vunamhung@gmail.com>
2020-10-02 10:29:19 +0700

    refactor

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,10 +1,10 @@
 import { FastifyRequest } from 'fastify';
 import fp from 'fastify-plugin';
 
-export default fp((server, options, done) => {
+export default fp((server, options, next) => {
   server.decorate('authenticate', async (request: FastifyRequest, reply) => {
     await request.jwtVerify().catch((error) => reply.send(error));
   });
 
-  done();
+  next();
 });
--- src/middlewares/isAdmin.ts
+++ src/middlewares/isAdmin.ts
@@ -1,12 +1,12 @@
 import { FastifyInstance, FastifyReply } from 'fastify';
 import fp from 'fastify-plugin';
 
-export default fp((server: FastifyInstance, options, done) => {
+export default fp((server: FastifyInstance, options, next) => {
   server.decorate('isAdmin', async (request, reply: FastifyReply) => {
     const { role } = server.token(request).user;
 
     if (role !== 'admin') reply.unauthorized('You are not allowed to make this request.');
   });
 
-  done();
+  next();
 });

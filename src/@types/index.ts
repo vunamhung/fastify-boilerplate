125d7feca90999fc96f95b90ef258d77672c6394
Hung Vu <vunamhung@gmail.com>
2020-10-02 23:56:16 +0700

    refactor

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,11 +1,12 @@
+import { preValidationHookHandler } from 'fastify';
 import { Messages } from 'mailgun-js';
 import { iToken } from '../utilities/token';
 
 declare module 'fastify' {
   export interface FastifyInstance {
+    authenticate: preValidationHookHandler;
+    isAdmin: preValidationHookHandler;
     mailgun: Messages;
-    authenticate: () => void;
-    token: (request: FastifyRequest) => iToken;
-    isAdmin: () => void;
+    token(request: FastifyRequest): iToken;
   }
 }

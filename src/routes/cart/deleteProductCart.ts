745c5a36c05f49b4e81ea324d3a2e8aad77f4b50
Hung Vu <vunamhung@gmail.com>
2020-10-03 18:09:48 +0700

    deleteProductCart.ts

---
+++ src/routes/cart/deleteProductCart.ts
@@ -0,0 +1,35 @@
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import Cart from '../../models/Cart';
+
+export default function (server: FastifyInstance, options, done) {
+  server.delete(
+    '/:cartId/delete/:productId',
+    {
+      schema: {
+        tags: ['cart'],
+        summary: 'Delete a product of cart.',
+        params: {
+          type: 'object',
+          properties: {
+            cardId: { type: 'string' },
+            productId: { type: 'string' },
+          },
+        },
+      },
+    },
+    async (request: FastifyRequest, reply) => {
+      try {
+        // @ts-ignore
+        const { cartId, productId } = request.params;
+
+        await Cart.findByIdAndUpdate(cartId, { $pull: { products: { product: productId } } });
+
+        reply.send({ success: true, message: 'Product removed.' });
+      } catch (err) {
+        reply.send(err);
+      }
+    },
+  );
+
+  done();
+}

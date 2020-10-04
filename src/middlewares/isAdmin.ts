6d7396efb4562bd7e40a1e5a3fe499e35bea4630
Hung Vu <vunamhung@gmail.com>
2020-10-04 12:01:35 +0700

    refactor

--- src/middlewares/isAdmin.ts
+++ src/middlewares/isAdmin.ts
@@ -5,7 +5,7 @@ export default fp((server: FastifyInstance, options, next) => {
   server.decorate('isAdmin', async (request, reply: FastifyReply) => {
     const { role } = server.token(request).user;
 
-    if (role !== 'admin') reply.unauthorized('You are not allowed to make this request.');
+    if (role !== 'admin' && role !== 'root') reply.unauthorized('You are not allowed to make this request.');
   });
 
   next();

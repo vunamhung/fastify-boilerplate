7a7312513114d420230c8803cca6914308b39763
Hung Vu <vunamhung@gmail.com>
2020-10-05 07:56:45 +0700

    fix order schema

--- src/models/Order.ts
+++ src/models/Order.ts
@@ -1,19 +1,44 @@
 import { Document, Schema, model } from 'mongoose';
 import { iCartModel } from './Cart';
 
-interface iOrderModel extends Document {
+export interface iOrderModel extends Document {
   cart: iCartModel;
   total: number;
   totalTax?: number;
 }
 
-const { Number } = Schema.Types;
+const { Number, String, ObjectId } = Schema.Types;
 
 const orderSchema = new Schema<iOrderModel>(
   {
     cart: {
-      type: Object,
-      required: true,
+      products: [
+        {
+          product: {
+            _id: {
+              type: ObjectId,
+              required: true,
+            },
+            name: {
+              type: String,
+              trim: true,
+              required: true,
+            },
+            sku: {
+              type: String,
+              required: true,
+            },
+            price: {
+              type: Number,
+              required: true,
+            },
+          },
+          quantity: {
+            type: Number,
+            required: true,
+          },
+        },
+      ],
     },
     total: {
       type: Number,

dac9e3c646287ae03046d270a91d8f18ce750b5c
Hung Vu <vunamhung@gmail.com>
2020-10-07 08:00:52 +0700

    improve upload

--- src/utilities/uploader.ts
+++ src/utilities/uploader.ts
@@ -1,35 +1,37 @@
 import fp from 'fastify-plugin';
 import { extname } from 'path';
 import { existsSync, mkdirSync } from 'fs';
-import { randomBytes } from 'crypto';
+import { uid } from 'rand-token';
 import multer, { contentParser, diskStorage } from 'fastify-multer';
-import { File } from 'fastify-multer/src/interfaces';
 
 const uploadDirs = {
-  png: 'public/uploads/images',
-  jpg: 'public/uploads/images',
-  jpeg: 'public/uploads/images',
-  gif: 'public/uploads/gifs',
-  doc: 'public/uploads/documents',
-  docx: 'public/uploads/documents',
+  'image/jpeg': 'public/uploads/images',
+  'image/png': 'public/uploads/images',
+  'image/webp': 'public/uploads/images',
+  'image/svg+xml': 'public/uploads/images',
+  'image/gif': 'public/uploads/gifs',
+  'application/msword': 'public/uploads/documents',
+  'application/pdf': 'public/uploads/documents',
+  'text/csv': 'public/uploads/csv',
   other: 'public/uploads/other-files',
 };
 
-const createRandomFileName = ({ originalname }: File) => {
-  const name: string = randomBytes(18).toString('hex');
+function createRandomFileName(originalname) {
+  const name: string = uid(16);
   const ext: string = extname(originalname).split('.')[1];
 
   return name + '.' + ext;
-};
+}
+
+function createDestination(mimetype) {
+  Object.keys(uploadDirs).forEach((dir) => !existsSync(uploadDirs[dir]) && mkdirSync(uploadDirs[dir], { recursive: true }));
+
+  return uploadDirs[mimetype] ? uploadDirs[mimetype] : uploadDirs['other'];
+}
 
 const storage = diskStorage({
-  destination: (req, { originalname }, cb) => {
-    Object.keys(uploadDirs).forEach((dir) => !existsSync(uploadDirs[dir]) && mkdirSync(uploadDirs[dir], { recursive: true }));
-
-    const ext = extname(originalname).split('.')[1];
-    cb(null, uploadDirs[ext] ? uploadDirs[ext] : uploadDirs['other']);
-  },
-  filename: (req, file, cb) => cb(null, createRandomFileName(file)),
+  destination: (req, { mimetype }, cb) => cb(null, createDestination(mimetype)),
+  filename: (req, { originalname }, cb) => cb(null, createRandomFileName(originalname)),
 });
 
 export const uploader = multer({ storage });

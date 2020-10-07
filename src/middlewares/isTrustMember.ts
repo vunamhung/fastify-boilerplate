3ad076a2a65adb6623f3f87869ac71725e17c5d4
Hung Vu <vunamhung@gmail.com>
2020-10-07 11:06:08 +0700

    refactor

--- src/middlewares/isTrustMember.ts
+++ src/middlewares/isTrustMember.ts
@@ -3,6 +3,8 @@ import fp from 'fastify-plugin';
 
 export default fp((server: FastifyInstance, options, next) => {
   server.decorate('isTrustMember', async (request, reply: FastifyReply) => {
+    await request.jwtVerify().catch((err) => reply.send(err));
+
     const { verified } = server.token(request).user;
 
     if (!verified) reply.unauthorized('Account not verified so you are not allowed to make this request.');

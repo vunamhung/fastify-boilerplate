58794938767f867b86287cafed95c9419bd18c26
Hung Vu <vunamhung@gmail.com>
2020-10-21 21:25:01 +0700

    use deep-email-validator to validate email

--- src/routes/auth/register.ts
+++ src/routes/auth/register.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify';
+import { validate } from 'deep-email-validator';
 import { uid } from 'rand-token';
 import User from '../../models/User';
 import { hashPassword } from '../../utilities';
@@ -27,9 +28,13 @@ export default function (server: FastifyInstance, options, done) {
       try {
         let user = await User.findOne({ email });
 
-        if (user) reply.badRequest('User already exists.');
+        if (user) return reply.badRequest('User already exists.');
+
+        const { valid, reason, validators } = await validate(email);
 
         user = new User({ email });
+
+        if (!valid) return reply.badRequest(validators[reason]?.reason ?? 'Please provide a valid email address.');
 
         user.password = await hashPassword(password);
         user.refreshToken = uid(64);

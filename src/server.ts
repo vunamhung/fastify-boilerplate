9a7b3933e6738d96b604e87bbf8968354cfdfe7a
Hung Vu <vunamhung@gmail.com>
2020-11-01 15:54:56 +0700

    refactor

--- src/server.ts
+++ src/server.ts
@@ -98,7 +98,7 @@ export default class {
   private registerHooks() {
     this.server.addHook('onRequest', (request, reply, done) => {
       if (request.headers.authorization?.split(' ')[1]) {
-        request.user = this.server.token(request).user;
+        request.user = this.server.token(request)?.user;
       }
 
       done();

b3b534616eec12567f0febd3779e272fbe6be636
Hung Vu <vunamhung@gmail.com>
2020-12-14 09:06:51 +0700

    refactor

--- src/routes/uploadFiles.ts
+++ src/routes/uploadFiles.ts
@@ -21,17 +21,19 @@ export default function (server: FastifyInstance, options, done) {
       // @ts-ignore
       const { files } = request;
 
+      const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, AWS_BUCKET_NAME } = process.env;
+
       const s3 = new AWS.S3({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION,
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY,
+        region: AWS_REGION,
       });
 
       files.map(async (file) => {
         await File.create(file);
         s3.putObject(
           {
-            Bucket: process.env.AWS_BUCKET_NAME,
+            Bucket: AWS_BUCKET_NAME,
             Key: file.originalname,
             Body: file.buffer,
             ContentType: file.mimetype,
--- src/services/mailgun.ts
+++ src/services/mailgun.ts
@@ -2,9 +2,11 @@ import fp from 'fastify-plugin';
 import mailgun from 'mailgun-js';
 
 export default fp((server, options, done) => {
+  const { MAILGUN_DOMAIN, MAILGUN_API_KEY } = process.env;
+
   const mg = mailgun({
-    domain: process.env.MAILGUN_DOMAIN,
-    apiKey: process.env.MAILGUN_API_KEY,
+    domain: MAILGUN_DOMAIN,
+    apiKey: MAILGUN_API_KEY,
   });
 
   server.decorate('mailgun', mg.messages());

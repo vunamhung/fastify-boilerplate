52e4d82220c2cc9dc3673fc75b0da444a1fc55b3
Hung Vu <vunamhung@gmail.com>
2023-10-03 07:05:00 +0700

    🔧 fix(redis.ts): cast Redis client to RedisClientType to improve type safety 📝 chore(redis.ts): update FastifyInstance interface to specify RedisClientType for redis property

--- src/plugins/redis.ts
+++ src/plugins/redis.ts
@@ -1,4 +1,5 @@
 import type { FastifyInstance } from 'fastify';
+import type { RedisClientType } from 'redis';
 import { env } from '~/utilities';
 import fp from 'fastify-plugin';
 import { createClient } from 'redis';
@@ -7,7 +8,7 @@ export default fp((fastify: FastifyInstance, _, done) => {
   const client = createClient({ url: env.REDIS_URL }).on('error', (err) => {
     console.log('Redis Client Error', err);
     fastify.log.error(err);
-  });
+  }) as RedisClientType;
 
   fastify.decorate('redis', client);
 
@@ -21,6 +22,6 @@ export default fp((fastify: FastifyInstance, _, done) => {
 
 declare module 'fastify' {
   export interface FastifyInstance {
-    redis: any;
+    redis: RedisClientType;
   }
 }

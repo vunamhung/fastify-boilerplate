460081f6b4ecd065ccca931a27310edfc4e6aaae
Hung Vu <vunamhung@gmail.com>
2023-11-19 00:02:22 +0700

    :recycle: Refactoring code.

--- src/plugins/emptyResponse.ts
+++ src/plugins/emptyResponse.ts
@@ -1,11 +1,11 @@
 import type { FastifyInstance } from 'fastify';
 import { convertType } from '~/utilities';
 import fp from 'fastify-plugin';
-import { isNilOrEmpty } from 'ramda-adjunct';
+import { isBlank } from 'ramda-adjunct';
 
 export default fp((fastify: FastifyInstance, _, done) => {
   fastify.addHook('onSend', async (_, reply, payload: any) => {
-    if ((isNilOrEmpty(convertType(payload)) && reply.statusCode === 200) || reply.statusCode === 204) {
+    if ((isBlank(convertType(payload)) && reply.statusCode === 200) || reply.statusCode === 204) {
       reply.code(204);
       return;
     }

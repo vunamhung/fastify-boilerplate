6eef4df3e0921aa038d9df8b9864462129b3d7c9
Hung Vu <vunamhung@gmail.com>
2024-01-05 00:06:31 +0700

    :recycle: Refactoring code.

--- src/plugins/redis.ts
+++ src/plugins/redis.ts
@@ -1,10 +1,10 @@
-import type { FastifyInstance } from 'fastify';
+import type { ZFastify } from '~/@types';
 import type { RedisClientType } from 'redis';
 import { env } from '~/utilities';
 import fp from 'fastify-plugin';
 import { createClient } from 'redis';
 
-export default fp((fastify: FastifyInstance, _, done) => {
+export default fp((fastify: ZFastify, _, done) => {
   const client = createClient({ url: env.REDIS_URL }).on('error', (err) => {
     console.log('Redis Client Error', err);
     fastify.log.error(err);
